c998b764ab4708d6b1914723650124183facc3db
nicklopez <nicklopez702@gmail.com>
2016-05-02 23:54:04 -0700

    Modified source query for map. JSON file updated with new data.

--- index.php
+++ index.php
@@ -15,6 +15,14 @@
                 padding: 0
             }
 
+            table {
+                border-collapse: collapse;
+            }
+
+            table, th, td {
+                border: 1px solid black;
+            }
+
             #mapdiv {
                 height: 100%
             }
@@ -38,28 +46,54 @@
                     $.each(json1, function(key, data) {
 
                         var latLng = new google.maps.LatLng(data.lat, data.lng);
-                        var iconImage = "";
-
-                        switch (data.year) {
-                            case "2010":
-                                iconImage = "img/red-dot.png";
-                                break;
-                            case "2011":
-                                iconImage = "img/blue-dot.png";
-                                break;
-                            case "2012":
-                                iconImage = "img/yellow-dot.png";
-                                break;
-                            case "2013":
-                                iconImage = "img/green-dot.png";
-                                break;
-                        }
+//                        var iconImage = "";
+//
+//                        switch (data.year) {
+//                            case "2010":
+//                                iconImage = "img/red-dot.png";
+//                                break;
+//                            case "2011":
+//                                iconImage = "img/blue-dot.png";
+//                                break;
+//                            case "2012":
+//                                iconImage = "img/yellow-dot.png";
+//                                break;
+//                            case "2013":
+//                                iconImage = "img/green-dot.png";
+//                                break;
+//                        }
 
                         // Create a marker and put it on the map
                         var marker = new google.maps.Marker({
-                            icon: iconImage,
-                            position: latLng,
-                            title: data.title + ' - Year: ' + data.year + ' - rowId: ' + data.objectid
+//                            icon: iconImage,
+                            position: latLng
+//                            title: data.title + ' - Year: ' + data.year + ' - rowId: ' + data.objectid
+                        });
+
+                        var contentString = '<div id="content">' +
+                            '<h1>Accident Data</h1>' +
+                            '<div id="bodyContent">' +
+                            '<p>Latitude: ' + data.lat + ', Longitude: ' + data.lng + '</p>' +
+                            '<table><tr><th>Year</th><th># of accidents at this location</th></tr>' +
+                            ((data.count2010 > 0) ? '<tr><td>2010</td><td style="text-align: center">' + data.count2010 + '</td></tr>' : '' ) +
+                            ((data.count2011 > 0) ? '<tr><td>2011</td><td style="text-align: center">' + data.count2011 + '</td></tr>' : '') +
+                            ((data.count2012 > 0) ? '<tr><td>2012</td><td style="text-align: center">' + data.count2012 + '</td></tr>' : '') +
+                            ((data.count2013 > 0) ? '<tr><td>2013</td><td style="text-align: center">' + data.count2013 + '</td></tr>' : '') +
+                            '</table>' +
+                            '<p></p>' +
+                            '</div>' +
+                            '</div>';
+
+                        var infowindow = new google.maps.InfoWindow({
+                            content: contentString
+                        });
+
+                        marker.addListener('mouseover', function() {
+                            infowindow.open(map, marker);
+                        });
+
+                        marker.addListener('mouseout', function() {
+                            infowindow.close();
                         });
 
 //                        latlngbounds.extend(latLng);
@@ -80,7 +114,7 @@
 //                box.addListener('mouseover', showAccidentData);
 
                 // Define an info window on the map.
-                infoWindow = new google.maps.InfoWindow();
+//                infoWindow = new google.maps.InfoWindow();
             }
 
             function showAccidentData() {
--- php/getCoordinates.php
+++ php/getCoordinates.php
@@ -19,25 +19,25 @@ $accidentData = array();
 //$sql1 = "SELECT CONCAT(ASTREET, ' & ', BSTREET) AS title, GIS_LAT AS lat, GIS_LONG AS lng FROM uhaRecords WHERE (ASTREET LIKE 'Coal' OR BSTREET LIKE 'Coal') OR (ASTREET LIKE 'lead' OR BSTREET LIKE 'lead')";
 //$sql2 = "SELECT year as title, count(*) as accidents FROM uhaRecords WHERE (ASTREET LIKE 'Coal' OR BSTREET LIKE 'Coal') OR (ASTREET LIKE 'lead' OR BSTREET LIKE 'lead') GROUP BY year";
 
-$sql1 = "SELECT objectid, CONCAT(ASTREET, ' & ', BSTREET) AS title, year, GIS_LAT AS lat, GIS_LONG AS lng FROM uhaRecords WHERE CITY = 'Albuquerque'";
-$sql2 = "SELECT year as title, count(*) as accidents FROM uhaRecords WHERE CITY = 'Albuquerque' GROUP BY year";
+$sql1 = "SELECT GIS_LAT AS lat, GIS_LONG AS lng,SUM(CASE WHEN (year='2010') THEN 1 ELSE 0 END) AS count2010, SUM(CASE WHEN (year='2011') THEN 1 ELSE 0 END) AS count2011, SUM(CASE WHEN (year='2012') THEN 1 ELSE 0 END) AS count2012, SUM(CASE WHEN (year='2013') THEN 1 ELSE 0 END) AS count2013 FROM uhaRecords GROUP BY GIS_LAT, GIS_LONG";
+//$sql2 = "SELECT year as title, count(*) as accidents FROM uhaRecords WHERE CITY = 'Albuquerque' GROUP BY year";
 
 $stmt1 = $pdo->query($sql1);
-$stmt2 = $pdo->query($sql2);
+//$stmt2 = $pdo->query($sql2);
 
 while($row1 = $stmt1->fetch(PDO::FETCH_ASSOC)) {
     $locations[] = $row1;
 }
 
-while($row2 = $stmt2->fetch(PDO::FETCH_ASSOC)) {
-    $accidentData[] = $row2;
-}
+//while($row2 = $stmt2->fetch(PDO::FETCH_ASSOC)) {
+//    $accidentData[] = $row2;
+//}
 
 $fp1 = fopen('locations.json', 'w');
 fwrite($fp1, json_encode($locations));
 fclose($fp1);
 
-$fp2 = fopen('accidents.json', 'w');
-fwrite($fp2, json_encode($accidentData));
-fclose($fp2);
+//$fp2 = fopen('accidents.json', 'w');
+//fwrite($fp2, json_encode($accidentData));
+//fclose($fp2);
 
