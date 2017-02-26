f64aae8d5efd32519251c9bad5bb9b5a07f21244
Royal Administrator at the IDSP <royaladmin@inter-dimensional-space-port.net>
2017-02-25 19:01:06 -0700

    specify database parameters in a distinct config file

--- php/getCoordinates.php
+++ php/getCoordinates.php
@@ -1,10 +1,8 @@
 <?php
-$servername = "127.0.0.1";
-$username = "";
-$password = "";
+require('uhaConfig.php');
 
 try {
-    $pdo = new PDO("mysql:host=$servername;dbname=uha", $username, $password);
+    $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     // set the PDO error mode to exception
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
---
+++ php/uhaConfig.php
@@ -0,0 +1,5 @@
+<?php
+$servername = "dbsites7-133";
+$username = "c130tfanalysis";
+$password = "505*Abq";
+$dbname = "c130uha_tfanalysis";
