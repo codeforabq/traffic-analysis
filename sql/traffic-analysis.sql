e7d91818f057cc99076785ea552af171439f455a
Brett Gilbert <bgilb13@gmail.com>
2017-02-25 18:53:34 -0700

    added the crash table via traffic-analysis.sql

---
+++ sql/traffic-analysis.sql
@@ -0,0 +1,11 @@
+CREATE TABLE crash (
+	crashId              INT UNSIGNED AUTO_INCREMENT NOT NULL,
+	crashCityId          INT UNSIGNED,
+	crashReportingAgency VARCHAR(255),
+	crashDateTime        DATETIME                    NOT NULL,
+	crashAddress         VARCHAR(255),
+	crashGeometry        POINT                       NOT NULL,
+	crashInjured         INT UNSIGNED,
+	crashKilled          INT UNSIGNED,
+	crashVehicles        INT UNSIGNED
+);
