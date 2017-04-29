76c2114b46b5f88b0defc093dccf3626cb7a5139
Valente Meza <kingracoon64@gmail.com>
2017-04-29 17:55:43 -0600

    mysql dataSources added

--- sql/datasources.sql
+++ sql/datasources.sql
@@ -6,3 +6,8 @@ CREATE TABLE `dataSources` (
 );
 
 INSERT INTO dataSources VALUES(1, "BernCo_Traffic_Accident_Data_2010_to_2013.csv");
+INSERT INTO dataSources VALUES(2, "primary-secondary-coal.csv.csv");
+INSERT INTO dataSources VALUES(3, "1994-2014.Primary.Secondary.Lead.csv");
+INSERT INTO dataSources VALUES(4, "1997-2014.Lead.Coal.Updated.csv");
+INSERT INTO dataSources VALUES(5, "Crashes_on_LeadCoal_EastWest.xlsx");
+INSERT INTO dataSources VALUES(6, "DR782_Albuquerque-2012-2015.csv");
