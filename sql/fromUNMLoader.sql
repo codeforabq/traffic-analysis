b576ad807b7535baf1d6b33c74f6b71d2e4d0216
Brian J. Stinar <brian@noventum.us>
2017-04-29 18:57:51 -0600

    Added source

--- sql/fromUNMLoader.sql
+++ sql/fromUNMLoader.sql
@@ -1,8 +1,8 @@
-source uhaStructureCreation.sql;
-
-LOAD DATA LOCAL INFILE 'BernCo_Traffic_Accident_Data_2010_to_2013.csv'
+LOAD DATA LOCAL INFILE '../datasets/fromUNM/BernCo_Traffic_Accident_Data_2010_to_2013.csv'
 INTO TABLE uhaRecords
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
-IGNORE 1 LINES;
+IGNORE 1 LINES
+SET SOURCEID = 1; 
+;
