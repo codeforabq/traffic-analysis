86a761cc66a35783b1c746fd09a625260c9109c3
Brian J. Stinar <brian@noventum.us>
2018-01-19 17:50:14 -0700

    loader changes + new dataset

--- sql/buildAll.bash
+++ sql/buildAll.bash
@@ -1,9 +1,9 @@
 #/bin/bash
 
-ROOTPW="password" # put your password here 
+ROOTPW="rateme12!" # put your password here 
 
 mysql -u root -p$ROOTPW "uha" -e "source uhaRecordsStructure.sql"; 
 mysql -u root -p$ROOTPW "uha" -e "source datasources.sql"; 
-
 mysql -u root -p$ROOTPW "uha" -e "source fromUNMLoader.sql"; 
+
 
--- sql/dataset2Loader.py
+++ sql/dataset2Loader.py
@@ -4,6 +4,7 @@ import pandas as pd
 import pprint
 import sys
 import csv
+import calendar
 
 results = [] 
 
@@ -13,17 +14,37 @@ if __name__ == "__main__":
         reader = csv.reader(inputFile, delimiter=',', quotechar='"')
         inHeaders = True 
         results = [] 
+        headers = reader.next()
+    
         for row in reader:
             if inHeaders:
-                headers = row
                 inHeaders = False
             else:
                 currentRowDict = {} 
                 count = 0 
                 for header in headers:
                     currentRowDict[header] = row[count]
+                    currentRowDict['SOURCEID'] = 2
                     count = count + 1
+                year = currentRowDict['CRASH DATE'].split('-')[0]
+                month = currentRowDict['CRASH DATE'].split('-')[1]
+                day = currentRowDict['CRASH DATE'].split('-')[2]
+                currentRowDict['DATE'] = month + "/" + day + "/" + year
+                currentRowDict['YEAR']= year # This makes me feel like reformatting the first dataset into a non-retarded format. Why am I transforming everything into a dumbasss format, for consistency?
+                currentRowDict['MONTH'] = calendar.month_name[int(month)]
+               
                 results.append(currentRowDict)
+
+
+# Let's start mapping results to what it needs to be in uha.uhaRecords
+# SOURCEID - 2
+# OBJECTID / REPORT - not sure, OBJECTID looks unique, REPORT is not. 
+# 
+
+
+# Remove "CRASH REPORT NUMBER" since I don't care, and it doesn't exactly map to OBJECTID / REPORTID 
+# AGENCY is a one-to-one mapping
+# CRASH DATE = YYYY-MM-DD, and needs to map to YYYY->DATE, MM->MONTH, DATE (M/D/YYYY)
 
 pp = pprint.PrettyPrinter(indent=4)
 pp.pprint(results)
